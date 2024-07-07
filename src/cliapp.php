ed4057188d20239f60738d47d9bb66457c492c45
SR-Sohan <sr.sohanur22@gmail.com>
2024-07-07 21:54:50 +0600

    install package

---
+++ src/cliapp.php
@@ -0,0 +1,10 @@
+<?php
+namespace App;
+
+use Symfony\Component\Console\Command\Command;
+
+Class CliApp extends Command {
+
+    
+
+}
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInitfc1cfc7796593413642ec756e41d2346
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInitfc1cfc7796593413642ec756e41d2346', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInitfc1cfc7796593413642ec756e41d2346', 'loadClassLoader'));
@@ -30,6 +32,18 @@ class ComposerAutoloaderInitfc1cfc7796593413642ec756e41d2346
         call_user_func(\Composer\Autoload\ComposerStaticInitfc1cfc7796593413642ec756e41d2346::getInitializer($loader));
 
         $loader->register(true);
+
+        $filesToLoad = \Composer\Autoload\ComposerStaticInitfc1cfc7796593413642ec756e41d2346::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
 
         return $loader;
     }
